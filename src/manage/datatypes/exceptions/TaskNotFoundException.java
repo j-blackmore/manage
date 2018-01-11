e141bbeb39b9880f5bf72b5e18d60693d1aca819
Joe <josephbroseph7@gmail.com>
2018-01-11 23:10:58 +0000

    Add custom TaskNotFoundException

---
+++ src/manage/datatypes/exceptions/TaskNotFoundException.java
@@ -0,0 +1,42 @@
+package manage.datatypes.exceptions;
+
+/**
+ * Exception which is thrown when task with a description could be found.
+ * 
+ * @author J Blackmore
+ */
+public class TaskNotFoundException extends Exception {
+
+    /** location where the task was trying to be located. */
+    private String searchLocation = null;
+
+    /**
+     * Creates a TaskNotFoundException where the message is the task description.
+     * 
+     * @param taskDesc message of the exception, the task description.
+     */
+    public TaskNotFoundException(String taskDesc) {
+        super(taskDesc);
+    }
+
+    /**
+     * Creates a TaskNotFoundException where the message is the task description.
+     * 
+     * @param taskDesc message of the exception, the task description.
+     * @param searchLocation the location the task was searched for.
+     */
+    public TaskNotFoundException(String taskDesc, String searchLocation) {
+        this(taskDesc);
+        this.searchLocation = searchLocation;
+    }
+
+    /**
+     * Returns the location of where the task that couldn't be found was searched for, 
+     * null if no location.
+     * 
+     * @return location where task was searched for.
+     */
+    public String getSearchLocation() {
+        return searchLocation;
+    }
+}
