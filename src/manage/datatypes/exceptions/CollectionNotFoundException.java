fccfef91fe874d5ed48bd3e78d390e43fa7b4a85
Joe <josephbroseph7@gmail.com>
2018-01-11 23:18:24 +0000

    Add custom CollectionNotFoundException

---
+++ src/manage/datatypes/exceptions/CollectionNotFoundException.java
@@ -0,0 +1,18 @@
+package manage.datatypes.exceptions;
+
+/**
+ * Exception which is thrown when collection with a name could be found.
+ * 
+ * @author J Blackmore
+ */
+public class CollectionNotFoundException extends Exception {
+
+    /**
+     * Creates a CollectionNotFoundException where the message is the collection name.
+     * 
+     * @param collectionName message of the exception, the collection name.
+     */
+    public CollectionNotFoundException(String collectionName) {
+        super(collectionName);
+    }
+}
