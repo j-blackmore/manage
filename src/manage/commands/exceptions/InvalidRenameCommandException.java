08dd772515fe3c30d7e27a7c43a708ee9803830f
Joe <josephbroseph7@gmail.com>
2018-01-25 18:51:58 +0000

    Add new exception for rename command and RenameCommand stub

---
+++ src/manage/commands/exceptions/InvalidRenameCommandException.java
@@ -0,0 +1,31 @@
+package manage.commands.exceptions;
+
+import manage.commands.RenameCommand;
+
+/**
+ * Exception which is thrown when an invalid rename command is created
+ * 
+ * @author J Blackmore
+ */
+public class InvalidRenameCommandException extends InvalidCommandException {
+    
+    /**
+     * Creates an InvalidRenameCommandException with an error message.
+     * 
+     * @param message error message of the exception.
+     */
+    public InvalidRenameCommandException(String message) {
+        super(message);
+    }
+
+    /**
+     * Creates an InvalidRenameCommandException from a Command.
+     * 
+     * @param command the command that caused the exception.
+     */
+    public InvalidRenameCommandException(RenameCommand command) {
+        this("Invalid rename command: \'" + command + "\', must follow format:\n" +
+             command.getCorrectCommandFormat());
+    }
+    
+}
