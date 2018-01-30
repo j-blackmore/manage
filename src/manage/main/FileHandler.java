fa3dbb963bbcad01c5de60f7713aefe19c853440
Joe <josephbroseph7@gmail.com>
2018-01-30 14:05:44 +0000

    Add functionality to save function, plus made use of setCompletionStatus methods

--- src/manage/main/FileHandler.java
+++ src/manage/main/FileHandler.java
@@ -1,10 +1,12 @@
 package manage.main;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import manage.datatypes.Collection;
 import manage.datatypes.Task;
@@ -108,11 +110,7 @@ public class FileHandler {
         Todo newTodo = new Todo(input.substring(2, input.indexOf(";")));
         input = input.substring(input.indexOf(";") + 1);
 
-        if(Boolean.parseBoolean(input.substring(0, input.indexOf(";")))) { 
-            newTodo.complete(); 
-        } else { 
-            newTodo.unComplete();
-        }
+        newTodo.setCompletionStatus(Boolean.parseBoolean(input.substring(0, input.indexOf(";"))));
         input = input.substring(input.indexOf(";") + 1);
         
         int numOfTasks = Integer.parseInt(input.substring(0, input.indexOf(";")));
@@ -133,11 +131,7 @@ public class FileHandler {
         Task newTask = new Task(input.substring(2, input.indexOf(";")));
         input = input.substring(input.indexOf(";") + 1);
 
-        if(Boolean.parseBoolean(input.substring(0, input.indexOf(";")))) {
-            newTask.complete();
-        } else {
-            newTask.unComplete();
-        }
+        newTask.setCompletionStatus(Boolean.parseBoolean(input.substring(0, input.indexOf(";"))));
 
         return newTask;
     }
@@ -145,9 +139,36 @@ public class FileHandler {
     /**
      * Overwrites the current save in file with the userProfile.
      * 
-     * @param userProfile the profile to save to file
+     * @param userProfile the profile to save.
      */
     public void save(Profile userProfile) {
-        // re-writes profile to file
+        BufferedWriter fileSave = null; 
+        
+        try {
+            fileSave = new BufferedWriter(new PrintWriter(filePath + fileName));
+            fileSave.write("0:" + userProfile.getUserName());
+
+            for(int i = 0; i < userProfile.collections.size(); i++) {
+                fileSave.write(userProfile.collections.get(i).getSaveFormat());
+            }
+
+            for(int j =0; j < userProfile.todos.size(); j++) {
+                fileSave.write(userProfile.todos.get(j).getSaveFormat());
+            }
+
+            for(int k = 0; k < userProfile.tasks.size(); k++) {
+                fileSave.write(userProfile.tasks.get(k).getSaveFormat());
+            }
+        } catch(IOException e) {
+            System.out.println("Error saving file, please try again");
+        } finally {
+            try {
+                if(fileSave != null) {
+                    fileSave.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error saving file, please try again");
+            }
+        }
     }
 }
